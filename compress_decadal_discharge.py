83eb522a7d8e2416baa309770a1875b4ed45b85b
rileyhales <rileyhales1@gmail.com>
2023-10-20 23:10:06 -0600

    revise zarr creation

--- compress_decadal_discharge.py
+++ compress_decadal_discharge.py
@@ -17,22 +17,21 @@ compression_options = {
     'shuffle': True,
 }
 
-for vpu_dir in [d for d in sorted(glob.glob('/Volumes/EB406_T7_2/geoglows2/retrospective_outputs/*')) if os.path.isdir(d)]:
+for vpu_dir in sorted([d for d in glob.glob('/Volumes/EB406_T7_2/geoglows2/v2_retrospective_outputs/614*') if os.path.isdir(d)]):
     vpu_number = os.path.basename(vpu_dir)
     logging.info(f'Processing VPU {vpu_number}')
 
     global_attributes = {
         'author': 'Riley Hales, PhD',
-        'title': f'GEOGloWS v2 Retrospective Discharge VPU {vpu_number}',
-        'institution': 'Brigham Young University',
-        'source': 'GEOGloWS v2',
-        'history': 'Created 2023-08-23',
+        'title': f'GEOGloWS v2 Retrospective Discharge',
+        'institution': 'Group on Earth Observations Global Water Sustainability Program',
+        'source': 'GEOGloWS Hydrologic Model v2',
+        'history': 'Created 2023-10-18',
         'references': 'https://geoglows.ecmwf.int/',
     }
 
     for decade in range(1940, 2020, 10):
         logging.info(f'Processing decade {decade}')
-        qout_files = glob.glob(os.path.join(vpu_dir, f'Qout_*{str(decade)[:3]}*.nc'))
         start_date = str(decade) + '0101'
         end_date = str(decade + 9) + '1231'
         out_file_path = os.path.join(vpu_dir, f'Qout_{vpu_number}_{start_date}_{end_date}.nc4')
@@ -42,6 +41,7 @@ for vpu_dir in [d for d in sorted(glob.glob('/Volumes/EB406_T7_2/geoglows2/retro
             continue
 
         # open the dataset and save it to a netcdf 4 format file with high compression
+        qout_files = glob.glob(os.path.join(vpu_dir, f'Qout_*_{str(decade)[:3]}*.nc'))
         with xr.open_mfdataset(qout_files) as ds:
             ds.attrs = global_attributes
             (

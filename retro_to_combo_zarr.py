69e7364a92bf16104cb64613cbfbeac691315f45
rileyhales <rileyhales1@gmail.com>
2023-10-20 23:10:20 -0600

    revise zarr creation

--- retro_to_combo_zarr.py
+++ retro_to_combo_zarr.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import sys
 
 import dask
@@ -15,51 +16,56 @@ logging.basicConfig(
 global_attributes = {
     'author': 'Riley Hales, PhD',
     'title': f'GEOGloWS v2 Retrospective Discharge',
-    'institution': 'Brigham Young University',
-    'source': 'GEOGloWS v2',
-    'history': 'Created 2023-10-13',
+    'institution': 'Group on Earth Observations Global Water Sustainability Program',
+    'source': 'GEOGloWS Hydrologic Model v2',
+    'history': 'Created 2023-10-18',
     'references': 'https://geoglows.ecmwf.int/',
 }
 
-with dask.config.set(**{'array.slicing.split_large_chunks': True}):
-    logging.info('opening dataset')
-    with xr.open_mfdataset('/Volumes/EB406_T7_2/geoglows2/v2_retrospective_outputs/*/Qout_*.nc4',
-                           concat_dim='rivid',
-                           combine='nested', ) as ds:
-        logging.info('dropping variables')
-        ds = ds.drop('crs').drop('Qout_err').drop('lat').drop('lon').drop('time_bnds')
-        ds.attrs = {
-            'author': 'Riley Hales, PhD',
-            'title': f'GEOGloWS v2 Retrospective Discharge',
-            'institution': 'Brigham Young University',
-            'source': 'GEOGloWS v2',
-            'history': 'Created 2023-10-13',
-            'references': 'https://geoglows.ecmwf.int/',
-        }
-        chunk_sizes = {
-            'time': ds.variables['time'].shape[0],
-            'rivid': "auto",
-        }
-        zarr_compressor = Blosc(cname='zstd', clevel=9, shuffle=Blosc.BITSHUFFLE)
-        zarr_enc = {
-            x: {'compressor': zarr_compressor} for x in ds.variables
-        }
-        logging.info('chunking')
-        ds = ds.chunk(chunk_sizes)
-        output_file = f'/Volumes/DrHalesT7/geoglows_v2_retrospective.zarr'
-        logging.info(f'Writing to {output_file}')
-        with dask.config.set(scheduler='threads'):
-            delayed_task = ds.to_zarr(
-                output_file,
-                zarr_version=2,
-                encoding=zarr_enc,
-                compute=False,
-            )
+# set up a progress bar
+progress = ProgressBar()
+progress.register()
 
-            # set up a progress bar
-            progress = ProgressBar()
-            progress.register()
+with dask.config.set(**{
+    'array.slicing.split_large_chunks': True,
+    'array.chunk-size': '75MB',
+}):
+    for decade in range(1940, 2020, 10):
+        logging.info(f'opening dataset {decade}')
+        # filter the dataset to only include the current year
+        output_file = f'/Volumes/DrHalesT7/geoglows_v2_retrospective_{decade}.zarr'
+        if os.path.exists(output_file):
+            logging.info(f'Skipping {output_file}')
+            continue
 
-            # compute the task
-            dask.compute(delayed_task)
-        logging.info('Done')
+        with xr.open_mfdataset(f'/Volumes/EB406_T7_2/geoglows2/v2_retrospective_outputs/*/Qout_*{decade}0101*.nc4',
+                               concat_dim='rivid',
+                               combine='nested', ) as ds:
+            logging.info('dropping variables')
+            ds = ds.drop('crs').drop('Qout_err').drop('lat').drop('lon').drop('time_bnds')
+            ds.attrs = global_attributes
+            chunk_sizes = {
+                'time': ds.variables['time'].shape[0],
+                'rivid': "auto",
+            }
+            zarr_compressor = Blosc(cname='zstd', clevel=9, shuffle=Blosc.BITSHUFFLE)
+            zarr_enc = {
+                x: {'compressor': zarr_compressor} for x in ds.variables
+            }
+            logging.info('chunking')
+            ds = ds.chunk(chunk_sizes)
+            logging.info(f'Writing to {output_file}')
+            with dask.config.set(scheduler='threads'):
+                delayed_task = (
+                    ds
+                    .to_zarr(
+                        output_file,
+                        zarr_version=2,
+                        encoding=zarr_enc,
+                        compute=False,
+                    )
+                )
+
+                # compute the task
+                dask.compute(delayed_task)
+                logging.info('Done')

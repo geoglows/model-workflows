1c37d2163b949ef84d68e623c7bd4f034006f9eb
Riley Hales PhD <39097632+rileyhales@users.noreply.github.com>
2024-01-29 14:54:55 -0700

    update zarr combination script

--- retro_to_combo_zarr.py
+++ retro_to_combo_zarr.py
@@ -18,8 +18,8 @@ global_attributes = {
     'title': f'GEOGloWS v2 Retrospective Discharge',
     'institution': 'Group on Earth Observations Global Water Sustainability Program',
     'source': 'GEOGloWS Hydrologic Model v2',
-    'history': 'Created 2023-10-18',
-    'references': 'https://geoglows.ecmwf.int/',
+    'history': 'Created 2024-01-29',
+    'references': 'https://geoglows.org/',
 }
 
 # set up a progress bar
@@ -28,17 +28,17 @@ progress.register()
 
 with dask.config.set(**{
     'array.slicing.split_large_chunks': True,
-    'array.chunk-size': '75MB',
+    'array.chunk-size': '5MB',
 }):
-    for decade in range(1940, 2020, 10):
+    for decade in range(1940, 2030, 10):
         logging.info(f'opening dataset {decade}')
         # filter the dataset to only include the current year
-        output_file = f'/Volumes/DrHalesT7/geoglows_v2_retrospective_{decade}.zarr'
+        output_file = f'/mnt/geoglows_v2_retrospective_{decade}.zarr'
         if os.path.exists(output_file):
             logging.info(f'Skipping {output_file}')
             continue
 
-        with xr.open_mfdataset(f'/Volumes/EB406_T7_2/geoglows2/v2_retrospective_outputs/*/Qout_*{decade}0101*.nc4',
+        with xr.open_mfdataset(f'/mnt/outputs/*/Qout_*_{str(decade)[:3]}*0101*.nc',
                                concat_dim='rivid',
                                combine='nested', ) as ds:
             logging.info('dropping variables')
@@ -69,3 +69,37 @@ with dask.config.set(**{
                 # compute the task
                 dask.compute(delayed_task)
                 logging.info('Done')
+
+    # combine the all-vpu-1-year-files into a single larger zarr file
+    logging.info('opening yearly zarr files')
+    with xr.open_mfdataset('/mnt/geoglows_v2_retrospective_*.zarr',
+                           concat_dim='time',
+                           combine='nested',
+                           parallel=True,
+                           engine='zarr', ) as ds:
+        ds.attrs = global_attributes
+        chunk_sizes = {
+            'time': 29220,  # hard pin at 80 years (1940-2019 inclusive)
+            'rivid': "auto",
+        }
+        zarr_compressor = Blosc(cname='zstd', clevel=9, shuffle=Blosc.BITSHUFFLE)
+        zarr_enc = {
+            x: {'compressor': zarr_compressor} for x in ds.variables
+        }
+        logging.info('chunking')
+        ds = ds.chunk(chunk_sizes)
+        logging.info('Writing to /mnt/geoglows_v2_retrospective.zarr')
+        with dask.config.set(scheduler='threads'):
+            delayed_task = (
+                ds
+                .to_zarr(
+                    '/mnt/geoglows_v2_retrospective.zarr',
+                    zarr_version=2,
+                    encoding=zarr_enc,
+                    compute=False,
+                )
+            )
+
+            # compute the task
+            dask.compute(delayed_task)
+            logging.info('Done')
